f93875aeacf3451d5d121447c7f49465d4352cb6
eniseguzel <sehereniseguzel@gmail.com>
2026-03-26 04:43:06 +0300

    TaskService and  dependecies is added

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,6 +1,6 @@
-package org.example.taskmanager.repository;
+package com.example.taskmanager.repository;
 
-import org.example.taskmanager.entity.Task;
+import com.example.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
